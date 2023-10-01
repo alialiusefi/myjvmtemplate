f49b77eaced204ef607b16b5e88a2307c62bfca7
alialiusefi <alighanem99@live.com>
2023-10-01 18:57:56 +0900

    java

---
+++ spring-boot-3/java-reactive/build.gradle.kts
@@ -0,0 +1,27 @@
+plugins {
+    java
+    id("org.springframework.boot") version "3.1.4"
+    id("io.spring.dependency-management") version "1.1.3"
+}
+
+group = "my.jvm"
+version = "0.0.1-SNAPSHOT"
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter")
+
+    testImplementation(platform("org.junit:junit-bom:5.9.1"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
---
+++ spring-boot-3/java-reactive/src/main/java/my/jvm/java/template/Application.java
@@ -0,0 +1,7 @@
+package my.jvm.java.template;
+
+public class Application {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
