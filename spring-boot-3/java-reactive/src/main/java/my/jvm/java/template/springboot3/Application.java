6d052047116ef9bab3f8917e2f764c94b82bead4
alialiusefi <alighanem99@live.com>
2023-10-09 14:16:11 +0900

    k8s configmap

--- spring-boot-3/java-reactive/src/main/java/my/jvm/java/template/springboot3/Application.java
+++ spring-boot-3/java-reactive/src/main/java/my/jvm/java/template/springboot3/Application.java
@@ -1,14 +1,25 @@
 package my.jvm.java.template.springboot3;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @EnableAsync
 public class Application {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
         SpringApplication.run(Application.class, args);
+    }
+
+    @Value("${print_me}")
+    private String printMe;
+
+    @EventListener(ApplicationReadyEvent.class)
+    public void doSomethingAfterStartup() {
+        System.out.println(printMe);
     }
 }
